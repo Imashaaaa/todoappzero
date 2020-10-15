e89fb9d22f878984e851b2e0ebde2db0c34693e0
sathiranipun <sathiranipun.vsn@gmail.com>
2020-10-16 01:59:35 +0530

    initial commit

---
+++ src/TodoComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TodoComponet = ({todos, deleteTodo}) => {
+
+    return(
+        todos.map(todo => {
+            return(
+                <div className="todo" key={todo.id}>
+                    <div>Description:{todo.desc}</div>
+                    <div>Time:{todo.time}</div>
+                    <div>Id:{todo.id} </div>
+                    <button onClick= {() => {deleteTodo(todo.id)}}>Delete</button>
+                </div>
+            )
+        })
+    )
+}
+
+export default TodoComponet;
